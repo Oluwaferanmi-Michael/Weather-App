bf0bcee950ed5c9b2541c15f14804c57f3603237
OluwaferanmiAj <oajirireloja@e86.ng>
2023-02-11 14:13:35 -0700

    Inherited Widget

---
+++ weather_app_inherited_widget/lib/pages/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+String title = "";
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            title = DateTime.now().toIso8601String();
+          });
+        },
+        child: Container(
+          color: Colors.white,
+        ),
+      )
+    );
+  }
+}
